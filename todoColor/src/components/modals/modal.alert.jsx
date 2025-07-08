0ff529907804162a355687fe57979772b76bfc3b
Lucasmenezes08 <lms4@cesar.school>
2025-07-07 22:08:26 -0300

    :lipstick: refactor: Estilizar componentes modals.

--- todoColor/src/components/modals/modal.alert.jsx
+++ todoColor/src/components/modals/modal.alert.jsx
@@ -3,16 +3,17 @@ import React, { useState } from "react";
 
 export default function AlertModal ({mensagem , onClose}){
     return createPortal (
-        <dialog open>
-            <section>
-                <h2>Alerta de erro!</h2>
-                <h3>{mensagem}</h3>
-            </section>
-            <section>
-                <button onClick={onClose}>Ok</button>   
-            </section>
-            
-            
-        </dialog> , document.getElementById('modal-alert')
+        <section className="fixed inset-0 z-50 bg-black/60">
+            <dialog open className="top-[calc(50%-130px)] left-[calc(50%-310px)] flex justify-center items-center rounded-2xl flex-col gap-6 w-[50%] h-[40vh]">
+                <section className="flex flex-col justify-center items-center gap-8">
+                    <h2 className="text-2xl">Alerta de erro!</h2>
+                    <h3 className="text-lg">{mensagem}</h3>
+                </section>
+                <section>
+                    <button className="w-[5rem] h-[3rem] bg-gray-200 rounded-2xl cursor-pointer hover:bg-gray-300 transition ease-in-out" onClick={onClose}>Ok</button>   
+                </section>
+            </dialog>
+        </section>
+         , document.getElementById('modal-alert')
     )
 }   
